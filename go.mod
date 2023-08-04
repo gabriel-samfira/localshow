53b290d09741ba3ac6896f7128ab79e2c59e0550
Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>
2023-08-04 16:04:03 +0300

    Initial commit
    Signed-off-by: Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/gabriel-samfira/localshow
+
+go 1.20
+
+require (
+	golang.org/x/crypto v0.11.0
+	golang.org/x/term v0.10.0
+)
+
+require golang.org/x/sys v0.10.0 // indirect
---
+++ main.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	//"encoding/json"
+	"os"
+
+	"golang.org/x/crypto/ssh"
+	terminal "golang.org/x/term"
+)
+
+type remoteForwardRequest struct {
+	BindAddr string
+	BindPort uint32
+}
+
+func main() {
+	// Public key authentication is done by comparing
+	// the public key of a received connection
+	// with the entries in the authorized_keys file.
+	authorizedKeysBytes, err := os.ReadFile("/home/gabriel/.ssh/authorized_keys")
+	if err != nil {
+		log.Fatalf("Failed to load authorized_keys, err: %v", err)
+	}
+
+	authorizedKeysMap := map[string]bool{}
+	for len(authorizedKeysBytes) > 0 {
+		pubKey, _, _, rest, err := ssh.ParseAuthorizedKey(authorizedKeysBytes)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		authorizedKeysMap[string(pubKey.Marshal())] = true
+		authorizedKeysBytes = rest
+	}
+
+	// An SSH server is represented by a ServerConfig, which holds
+	// certificate details and handles authentication of ServerConns.
+	config := &ssh.ServerConfig{
+		// Remove to disable password auth.
+		PasswordCallback: func(c ssh.ConnMetadata, pass []byte) (*ssh.Permissions, error) {
+			// Should use constant-time compare (or better, salt+hash) in
+			// a production setting.
+			if c.User() == "testuser" && string(pass) == "tiger" {
+				return nil, nil
+			}
+			return nil, fmt.Errorf("password rejected for %q", c.User())
+		},
+
+		// Remove to disable public key auth.
+		PublicKeyCallback: func(c ssh.ConnMetadata, pubKey ssh.PublicKey) (*ssh.Permissions, error) {
+			if authorizedKeysMap[string(pubKey.Marshal())] {
+				return &ssh.Permissions{
+					// Record the public key used for authentication.
+					Extensions: map[string]string{
+						"pubkey-fp": ssh.FingerprintSHA256(pubKey),
+					},
+				}, nil
+			}
+			return nil, fmt.Errorf("unknown public key for %q", c.User())
+		},
+	}
+
+	privateBytes, err := os.ReadFile("/home/gabriel/personal/tunnelicious/ssh_host_rsa_key")
+	if err != nil {
+		log.Fatal("Failed to load private key: ", err)
+	}
+
+	private, err := ssh.ParsePrivateKey(privateBytes)
+	if err != nil {
+		log.Fatal("Failed to parse private key: ", err)
+	}
+
+	config.AddHostKey(private)
+
+	// Once a ServerConfig has been configured, connections can be
+	// accepted.
+	listener, err := net.Listen("tcp", "0.0.0.0:2022")
+	if err != nil {
+		log.Fatal("failed to listen for connection: ", err)
+	}
+	nConn, err := listener.Accept()
+	if err != nil {
+		log.Fatal("failed to accept incoming connection: ", err)
+	}
+
+	// Before use, a handshake must be performed on the incoming
+	// net.Conn.
+	conn, chans, reqs, err := ssh.NewServerConn(nConn, config)
+	if err != nil {
+		log.Fatal("failed to handshake: ", err)
+	}
+	log.Printf("logged in with key %s", conn.Permissions.Extensions["pubkey-fp"])
+
+	// The incoming Request channel must be serviced.
+	go func() {
+		for {
+			req := <-reqs
+			switch req.Type {
+			case "tcpip-forward":
+				var reqPayload remoteForwardRequest
+				if err := ssh.Unmarshal(req.Payload, &reqPayload); err != nil {
+					log.Print(err)
+					continue
+				}
+				fmt.Println(reqPayload.BindAddr)
+				fmt.Println(reqPayload.BindPort)
+			}
+		}
+	}()
+	// Service the incoming Channel channel.
+	for newChannel := range chans {
+		// Channels have a type, depending on the application level
+		// protocol intended. In the case of a shell, the type is
+		// "session" and ServerShell may be used to present a simple
+		// terminal interface.
+		if newChannel.ChannelType() != "session" {
+			newChannel.Reject(ssh.UnknownChannelType, "unknown channel type")
+			continue
+		}
+		channel, requests, err := newChannel.Accept()
+		if err != nil {
+			log.Fatalf("Could not accept channel: %v", err)
+		}
+
+		// Sessions have out-of-band requests such as "shell",
+		// "pty-req" and "env".  Here we handle only the
+		// "shell" request.
+		go func(in <-chan *ssh.Request) {
+			for req := range in {
+				req.Reply(req.Type == "shell", nil)
+			}
+		}(requests)
+
+		term := terminal.NewTerminal(channel, "> ")
+
+		go func() {
+			defer channel.Close()
+			for {
+				line, err := term.ReadLine()
+				if err != nil {
+					break
+				}
+				fmt.Printf("%s\r\n", line)
+			}
+		}()
+	}
+}
