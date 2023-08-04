9bf529405e12a14a2667738727ab8a5e4b6ce292
Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>
2023-08-04 17:44:42 +0300

    Generate key, handle tcpip-forward
    Signed-off-by: Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	"net"
+	"strconv"
 
 	//"encoding/json"
 	"os"
@@ -12,59 +19,98 @@ import (
 	terminal "golang.org/x/term"
 )
 
+const (
+	forwardedTCPChannelType = "forwarded-tcpip"
+)
+
 type remoteForwardRequest struct {
 	BindAddr string
 	BindPort uint32
 }
 
+type remoteForwardChannelData struct {
+	DestAddr   string
+	DestPort   uint32
+	OriginAddr string
+	OriginPort uint32
+}
+
+func GenerateKey(pth string) error {
+	if _, err := os.Stat(pth); err == nil {
+		return nil
+	}
+	rsaKey, err := rsa.GenerateKey(rand.Reader, 4096)
+	if err != nil {
+		return err
+	}
+
+	keyOut, err := os.OpenFile(pth, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return fmt.Errorf("failed to open %s for writing: %w", pth, err)
+	}
+
+	pemBlock := &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(rsaKey)}
+	if err := pem.Encode(keyOut, pemBlock); err != nil {
+		return fmt.Errorf("failed to write data to %s: %s", pth, err)
+	}
+
+	return nil
+}
+
 func main() {
+	if err := GenerateKey("/tmp/testcert"); err != nil {
+		log.Fatal(err)
+	}
 	// Public key authentication is done by comparing
 	// the public key of a received connection
 	// with the entries in the authorized_keys file.
-	authorizedKeysBytes, err := os.ReadFile("/home/gabriel/.ssh/authorized_keys")
-	if err != nil {
-		log.Fatalf("Failed to load authorized_keys, err: %v", err)
-	}
+	// authorizedKeysBytes, err := os.ReadFile("/home/gabriel/.ssh/authorized_keys")
+	// if err != nil {
+	// 	log.Fatalf("Failed to load authorized_keys, err: %v", err)
+	// }
 
-	authorizedKeysMap := map[string]bool{}
-	for len(authorizedKeysBytes) > 0 {
-		pubKey, _, _, rest, err := ssh.ParseAuthorizedKey(authorizedKeysBytes)
-		if err != nil {
-			log.Fatal(err)
-		}
+	// authorizedKeysMap := map[string]bool{}
+	// for len(authorizedKeysBytes) > 0 {
+	// 	pubKey, _, _, rest, err := ssh.ParseAuthorizedKey(authorizedKeysBytes)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
 
-		authorizedKeysMap[string(pubKey.Marshal())] = true
-		authorizedKeysBytes = rest
-	}
+	// 	authorizedKeysMap[string(pubKey.Marshal())] = true
+	// 	authorizedKeysBytes = rest
+	// }
 
 	// An SSH server is represented by a ServerConfig, which holds
 	// certificate details and handles authentication of ServerConns.
 	config := &ssh.ServerConfig{
 		// Remove to disable password auth.
-		PasswordCallback: func(c ssh.ConnMetadata, pass []byte) (*ssh.Permissions, error) {
-			// Should use constant-time compare (or better, salt+hash) in
-			// a production setting.
-			if c.User() == "testuser" && string(pass) == "tiger" {
-				return nil, nil
-			}
-			return nil, fmt.Errorf("password rejected for %q", c.User())
-		},
-
-		// Remove to disable public key auth.
-		PublicKeyCallback: func(c ssh.ConnMetadata, pubKey ssh.PublicKey) (*ssh.Permissions, error) {
-			if authorizedKeysMap[string(pubKey.Marshal())] {
-				return &ssh.Permissions{
-					// Record the public key used for authentication.
-					Extensions: map[string]string{
-						"pubkey-fp": ssh.FingerprintSHA256(pubKey),
-					},
-				}, nil
-			}
-			return nil, fmt.Errorf("unknown public key for %q", c.User())
-		},
+		NoClientAuth:     true,
+		PasswordCallback: nil,
+		// PasswordCallback: func(c ssh.ConnMetadata, pass []byte) (*ssh.Permissions, error) {
+		// 	// allow everyone. This is a demo.
+		// 	return &ssh.Permissions{
+		// 		// Record the public key used for authentication.
+		// 		Extensions: map[string]string{
+		// 			"password": "",
+		// 		},
+		// 	}, nil
+		// },
+		PublicKeyCallback: nil,
+		// // Remove to disable public key auth.
+		// PublicKeyCallback: func(c ssh.ConnMetadata, pubKey ssh.PublicKey) (*ssh.Permissions, error) {
+		// 	if authorizedKeysMap[string(pubKey.Marshal())] {
+		// 		return &ssh.Permissions{
+		// 			// Record the public key used for authentication.
+		// 			Extensions: map[string]string{
+		// 				"pubkey-fp": ssh.FingerprintSHA256(pubKey),
+		// 			},
+		// 		}, nil
+		// 	}
+		// 	return nil, fmt.Errorf("unknown public key for %q", c.User())
+		// },
 	}
 
-	privateBytes, err := os.ReadFile("/home/gabriel/personal/tunnelicious/ssh_host_rsa_key")
+	privateBytes, err := os.ReadFile("/tmp/testcert")
 	if err != nil {
 		log.Fatal("Failed to load private key: ", err)
 	}
@@ -82,70 +128,161 @@ func main() {
 	if err != nil {
 		log.Fatal("failed to listen for connection: ", err)
 	}
-	nConn, err := listener.Accept()
-	if err != nil {
-		log.Fatal("failed to accept incoming connection: ", err)
-	}
-
-	// Before use, a handshake must be performed on the incoming
-	// net.Conn.
-	conn, chans, reqs, err := ssh.NewServerConn(nConn, config)
-	if err != nil {
-		log.Fatal("failed to handshake: ", err)
-	}
-	log.Printf("logged in with key %s", conn.Permissions.Extensions["pubkey-fp"])
-
-	// The incoming Request channel must be serviced.
-	go func() {
-		for {
-			req := <-reqs
-			switch req.Type {
-			case "tcpip-forward":
-				var reqPayload remoteForwardRequest
-				if err := ssh.Unmarshal(req.Payload, &reqPayload); err != nil {
-					log.Print(err)
-					continue
-				}
-				fmt.Println(reqPayload.BindAddr)
-				fmt.Println(reqPayload.BindPort)
-			}
+	for {
+		nConn, err := listener.Accept()
+		if err != nil {
+			log.Printf("failed to accept incoming connection: %s", err)
+			return
 		}
-	}()
-	// Service the incoming Channel channel.
-	for newChannel := range chans {
-		// Channels have a type, depending on the application level
-		// protocol intended. In the case of a shell, the type is
-		// "session" and ServerShell may be used to present a simple
-		// terminal interface.
-		if newChannel.ChannelType() != "session" {
-			newChannel.Reject(ssh.UnknownChannelType, "unknown channel type")
+
+		// Before use, a handshake must be performed on the incoming
+		// net.Conn.
+		conn, chans, reqs, err := ssh.NewServerConn(nConn, config)
+		if err != nil {
+			log.Printf("failed to handshake: %s", err)
 			continue
 		}
-		channel, requests, err := newChannel.Accept()
-		if err != nil {
-			log.Fatalf("Could not accept channel: %v", err)
-		}
+		log.Printf("logged in with permissions %v", conn.Permissions)
 
-		// Sessions have out-of-band requests such as "shell",
-		// "pty-req" and "env".  Here we handle only the
-		// "shell" request.
-		go func(in <-chan *ssh.Request) {
-			for req := range in {
-				req.Reply(req.Type == "shell", nil)
-			}
-		}(requests)
-
-		term := terminal.NewTerminal(channel, "> ")
-
+		quit := make(chan struct{})
+		// The incoming Request channel must be serviced.
 		go func() {
-			defer channel.Close()
 			for {
-				line, err := term.ReadLine()
-				if err != nil {
-					break
+				select {
+				case req := <-reqs:
+					if req == nil {
+						break
+					}
+					switch req.Type {
+					case "tcpip-forward":
+						var reqPayload remoteForwardRequest
+						if err := ssh.Unmarshal(req.Payload, &reqPayload); err != nil {
+							log.Print(err)
+							continue
+						}
+
+						go func(reqPayload remoteForwardRequest) {
+							// Allocate a random port. We'll lie to the client that we actually
+							// bound to the requested port.
+							ln, err := net.Listen("tcp", "127.0.11.1:0")
+							if err != nil {
+								log.Printf("failed to listen: %s", err)
+								return
+							}
+
+							go func() {
+								<-quit
+								ln.Close()
+							}()
+							destPort := ln.Addr().(*net.TCPAddr).Port
+							log.Printf("actual port is: %d", destPort)
+							log.Printf("listening on %s:%d", reqPayload.BindAddr, reqPayload.BindPort)
+							c, err := ln.Accept()
+							if err != nil {
+								if !errors.Is(err, net.ErrClosed) {
+									log.Printf("failed to accept: %s", err)
+								}
+								return
+							}
+
+							log.Printf("accepted connection from %s", c.RemoteAddr())
+							originAddr, orignPortStr, _ := net.SplitHostPort(c.RemoteAddr().String())
+							originPort, _ := strconv.Atoi(orignPortStr)
+							payload := ssh.Marshal(&remoteForwardChannelData{
+								DestAddr: reqPayload.BindAddr,
+								// Not the actual port we're listening on.
+								DestPort:   uint32(reqPayload.BindPort),
+								OriginAddr: originAddr,
+								OriginPort: uint32(originPort),
+							})
+
+							go func() {
+								ch, reqs, err := conn.OpenChannel(forwardedTCPChannelType, payload)
+								if err != nil {
+									// TODO: log failure to open channel
+									log.Println(err)
+									c.Close()
+									return
+								}
+								defer ch.Close()
+								defer c.Close()
+								go func() {
+									for {
+										select {
+										case <-quit:
+											return
+										case req := <-reqs:
+											if req == nil {
+												break
+											}
+											log.Printf("Got request of type: %s", req.Type)
+										}
+									}
+								}()
+								go func() {
+									io.Copy(ch, c)
+								}()
+								go func() {
+									io.Copy(c, ch)
+								}()
+								<-quit
+							}()
+							<-quit
+						}(reqPayload)
+					default:
+						log.Printf("unexpected request type: %s", req.Type)
+					}
+				case <-quit:
+					log.Printf("closing connection from %s", conn.RemoteAddr())
+					return
 				}
-				fmt.Printf("%s\r\n", line)
 			}
 		}()
+		// Service the incoming Channel channel.
+		for newChannel := range chans {
+			// Channels have a type, depending on the application level
+			// protocol intended. In the case of a shell, the type is
+			// "session" and ServerShell may be used to present a simple
+			// terminal interface.
+			if newChannel.ChannelType() != "session" {
+				newChannel.Reject(ssh.UnknownChannelType, "unknown channel type")
+				continue
+			}
+			channel, requests, err := newChannel.Accept()
+			if err != nil {
+				log.Fatalf("Could not accept channel: %v", err)
+			}
+
+			// Sessions have out-of-band requests such as "shell",
+			// "pty-req" and "env".  Here we handle only the
+			// "shell" request.
+			go func(in <-chan *ssh.Request) {
+				for req := range in {
+					switch req.Type {
+					case "shell":
+						req.Reply(req.Type == "shell", nil)
+					default:
+						log.Printf("unexpected request type: %s", req.Type)
+					}
+				}
+			}(requests)
+
+			term := terminal.NewTerminal(channel, "> ")
+
+			go func() {
+				defer channel.Close()
+				defer conn.Close()
+				for {
+					line, err := term.ReadLine()
+					if err != nil {
+						break
+					}
+					fmt.Printf("%s\r\n", line)
+				}
+			}()
+		}
+		nConn.Close()
+		close(quit)
+		log.Printf("closed connection from %s", conn.RemoteAddr())
 	}
 }
